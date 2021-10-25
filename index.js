bf094e3c7789cd33ba7deb56a01c8c635b48b13d
leanhvu21042001@gmail.com <leanhvu1412>
2021-10-25 16:41:50 +0700

    Set up error handler, cookie parser, cors, body parser

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express')
+const cors = require('cors')
+const corsOptions = require('./src/config/corsOptions');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
 
 // Create application instance
 const app = express()
 
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// set up for cookies middleware
+app.use(cookieParser());
 
 // routes
 app.use("/api", require('./src/routes/index.routes'));
 
+// handler errors
+app.all('*', (req, res) => {
+  return res.status(404).json({ mgs: "Error 404", success: false });
+})
+app.use(errorHandler);
 
 const port = process.env.PORT || 3500;
 app.listen(
---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,6 @@
+const errorHandler = (err, req, res) => {
+  console.error(err.stack);
+  return res.status(500).json({ message: err.message });
+}
+
+module.exports = { errorHandler };
