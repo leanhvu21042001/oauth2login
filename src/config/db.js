282c776fe9145574c8d89fde0869a756e2def0a9
leanhvu21042001@gmail.com <leanhvu1412>
2021-10-25 15:47:11 +0700

    Setup database

---
+++ src/config/db.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "poolConfig":{
+      "host": "localhost",
+      "port" : 3306,
+      "user" : "root",
+      "password" : "",
+      "database" : "blog_web1",
+      "connectionLimit" : 100
+  }
+}
---
+++ src/utils/db.js
@@ -0,0 +1,84 @@
+const mysql = require('mysql');
+const dbConfig = require('./../config/db');
+
+const pool = mysql.createPool(dbConfig.poolConfig);
+
+
+// Cơ chết promies
+module.exports = {
+  load: function (sql) {
+    return new Promise((resolve, reject) => {
+      pool.query(sql, (err, results) => {
+        if (err) {
+          console.error(err)
+          return reject(err);
+        }
+        resolve(results);
+      });
+    });
+  },
+  insert: function (table, entity) {
+
+    const sql = `INSERT INTO ${table} SET ?`;
+    return new Promise((resolve, reject) => {
+      pool.query(sql, entity, (err, results) => {
+        if (err) {
+          console.error(err)
+          return reject(err);
+        }
+        resolve(results);
+      });
+    })
+  },
+  update: function (table, entity, condition) {
+    const sql = `UPDATE ${table} SET ? WHERE ?`;
+
+    return new Promise((resolve, reject) => {
+      pool.query(sql, [entity, condition], (err, results, fields) => {
+        if (err) {
+          console.error(err)
+          return reject(err);
+        }
+        resolve(results);
+      });
+    })
+  },
+  delete: function (table, condition) {
+    const sql = `DELETE FROM ${table} WHERE ?`;
+    return new Promise((resolve, reject) => {
+      pool.query(sql, condition, (err, results) => {
+        if (err) {
+          console.error(err)
+          return reject(err);
+        }
+        resolve(results);
+      });
+    })
+  },
+  update2condition: function (table, entity, condition1, condition2) {
+    const sql = `UPDATE ${table} SET ? WHERE ? AND ?`;
+    console.log(entity, "Cond: ", condition1, condition2);
+    return new Promise((resolve, reject) => {
+      pool.query(sql, [entity, condition1, condition2], (err, results, fields) => {
+        
+        if (err) {
+          console.error(err)
+          return reject(err);
+        }
+        resolve(results);
+      });
+    })
+  },
+  delete2condition: function (table, condition1, condition2) {
+    const sql = `DELETE FROM ${table} WHERE ? AND ?`;
+    return new Promise((resolve, reject) => {
+      pool.query(sql, [condition1, condition2], (err, results) => {
+        if (err) {
+          console.error(err)
+          return reject(err);
+        }
+        resolve(results);
+      });
+    })
+  },
+}
